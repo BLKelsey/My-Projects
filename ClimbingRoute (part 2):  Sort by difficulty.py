121d48c56acb863d05eb52a045a872ee8c3dd3f8
BLKelsey <blkelsey@gmail.com>
2025-11-07 15:28:06 -0500

    Self learning Python - 1st push

---
+++ ClimbingRoute (part 2):  Sort by difficulty.py
@@ -0,0 +1,23 @@
+class ClimbingRoute:
+    def __init__(self, name, length, grade):
+        self.name = name
+        self.length = length
+        self.grade = grade
+
+    def __str__(self):
+        return f" Route: {self.name}, length: {self.length}ft, grade: {self.grade}"
+
+def sort_by_difficulty(routes):
+    sorted_routes = sorted(routes, key=lambda route: route.grade, reverse=True)
+    return sorted_routes
+
+
+r1 = ClimbingRoute("Edge", 38, "6A+")
+r2 = ClimbingRoute("Smooth operator", 11, "7A")
+r3 = ClimbingRoute("Synchro", 14, "8C+")
+r4 = ClimbingRoute("Small steps", 12, "6A+")
+
+print()
+routes = [r1, r2, r3, r4]
+for route in sort_by_difficulty(routes):
+    print(route)
