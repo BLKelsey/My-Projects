9c3e251aa8527fc4e0802dae8d90436bce054fa5
BLKelsey <blkelsey@gmail.com>
2025-11-08 17:30:28 -0500

    Self learning Python - 1st push

---
+++ Climbing areas (part2):  Sort by difficulty.py
@@ -0,0 +1,40 @@
+class ClimbingRoute:
+    def __init__(self, name, length, grade):
+        self.name = name
+        self.length = length
+        self.grade = grade
+
+    def __str__(self):
+        return f"{self.name}, length {self.length} feet, grade {self.grade}"
+
+class ClimbingArea:
+    def __init__(self, name):
+        self.name = name
+        self.routes = []  # holds ClimbingRoute objects
+
+    def add_route(self, route):
+        self.routes.append(route)
+
+    def routes_count(self):
+        # returns the number of routes in this climbing area
+        return len(self.routes)
+
+    def __str__(self):
+         # find the longest route and summarize
+         hardest = max(self.routes, key=lambda route: route.grade)
+         return f"{self.name}, {len(self.routes)} , {self.grade}"
+
+def sort_by_difficulty(routes: list):
+    # Sort routes by their difficulty
+    sorted_routes = sorted(routes, key=lambda route: route.grade, reverse=True)
+    return sorted_routes
+
+r1 = ClimbingRoute("Edge", 38, "6A+")
+r2 = ClimbingRoute("Smooth operator", 11, "7A")
+r3 = ClimbingRoute("Synchro", 14, "8C+")
+r4 = ClimbingRoute("Small steps", 12, "6A+")
+
+routes = [r1, r2, r3, r4]
+print()
+for route in sort_by_difficulty(routes):
+    print(route)
