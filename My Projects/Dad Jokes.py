7992b0b0918c5d35e958148d7245c1f56caf9287
BLKelsey <blkelsey@gmail.com>
2026-01-03 16:25:38 -0500

    Self learning Python - 1st push

---
+++ My Projects/Dad Jokes.py
@@ -0,0 +1,31 @@
+import random
+
+jokes = [
+    "What kind of shoes to frogs wear?",
+    "I'm afraid for the calendar. Its days are numbered.",
+    "Singing in the shower is fun until you get soap in your mouth. Then it's a soap opera.",
+    "When two vegans get in an argument, is it still called a beef?",
+    "Do you wanna box for your leftovers? No, but I'll wrestle you for them!",
+    "That car looks nice but the muffler seems exhausted.",
+    "If a child refuses to nap, are they guilty of resisting a rest?",
+    "A cheeseburger walks into a bar. The bartender says, 'Sorry, we don't serve food here.",
+    "I used to play piano by ear. Now I use my hands.",
+    "I made a pencil with two erasers. It was pointless.",
+    "You know, people say they pick their nose, but I feel like I was just born with mine!",
+    "If you see a crime at an Apple Store, does that make you an iWitness?",
+    " I don’t trust stairs. They’re always up to something!",
+    " I used to be addicted to the hokey pokey, but then I turned myself around.",
+    "The football coach went to the bank to get his quarterback.",
+    "My dream job is to clean mirrors, because I can really see myself doing that!",
+    "At first, I thought my chiropractor wasn’t any good, but now I stand corrected.",
+    "I'd avoid the sushi if I were you — it's a little fishy!",
+    "Mom asked me to put ketchup on the grocery list, and now I can't read what else is on it!.",
+    ]
+
+input("Hit ENTER to get a random joke!")
+random.choice(jokes)
+print()
+
+print(random.choice(jokes))
+
+
