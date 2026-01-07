785b97ecc97da6217ef9a204bfddf47a6be737d0
BLKelsey <blkelsey@gmail.com>
2026-01-07 15:34:27 -0500

    Self learning Python - 1st push

--- My Projects/Index position explainer tool.py
+++ My Projects/Index position explainer tool.py
@@ -22,7 +22,7 @@ for item in items:                     # Loop through each item in the list
 
 print("Choose an item number:")         # Ask the user to choose an item
 user_number = int(input("> "))          # Get user input and convert it to an integer
-index_pos = user_number - 1             # Convert the user's number choice (starts at 1) to a list index (starts at 0)
+index_pos = user_number - 1             # Convert the user's number choice (starts at 1) to a list index which actually (starts at 0)
 
 if index_pos < 0 or index_pos >= len(items):   # Check if index is out of range
     print("That number is out of range")        # Warn the user
